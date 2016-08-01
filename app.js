bcc8135795f9fd14a8641d9d8ce6ad2121e65627
lionnel <lionnel@avril.co>
2016-08-01 16:00:28 +0200

    Webpack and configs initialized

--- app.js
+++ app.js
@@ -10,17 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser());
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -39,7 +36,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -50,7 +47,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 var ListController = require('./../controllers/ListController');
+
+router.get('/', function(req, res, next) {
+	res.setHeader('Content-type','text/html');
+	res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
 
 /* GET home page. */
 router.get('/invoices', function(req, res, next) {
