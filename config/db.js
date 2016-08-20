48f32ee6daaaaf7268630e61498936884d51c797
lionnel <lionnel@avril.co>
2016-08-20 16:20:29 +0200

    [PDS] refresh commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 if (process.env.NODE_ENV == "production")
-	mongoose.connect("mongodb://lasalle33:J1Z8K0G6C1G8N6@ds063715.mlab.com:63715/elodidel-db");
+	mongoose.connect("mongodb://elodidel:J1Z8K0G6C1G8N6@ds063715.mlab.com:63715/elodidel-db");
 else
 	mongoose.connect('mongodb://localhost/ELODIE');
 
